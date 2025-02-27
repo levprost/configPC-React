220e2a6f24572e0938ac3596c979d00d646692c2
levprost <smolev17@gmail.com>
2025-02-27 15:55:21 +0100

    NavBar and Axios installed

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Menu'; 
+ 
+const Home = () => { 
+    return ( 
+        <div> 
+            <Navbar/>
+            <h1>Bienvenu sur la page d'accueil</h1> 
+        </div> 
+    ); 
+}; 
+ 
+export default Home; 
